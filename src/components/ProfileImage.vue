901bc556da73811349acb14397afc16d2143a25e
Ricard Bargallo <ricard.bargallo.m@gmail.com>
2022-03-16 21:30:28 +0100

    Iteration 3

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="src" className="profile" alt="profile" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span className="user">
+      <span className="name">{{ userData.name }}</span>
+      <span className="handle">@{{ userData.handle }}</span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: {
+    userData: Object,
+  },
+};
+</script>
+
+<style>
+</style>
